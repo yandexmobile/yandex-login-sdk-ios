fe4941e194f24a5631d1fcb3c417db5828f74ff4
Ivan Alekseev <alivig1@yandex-team.ru>
2023-04-14 18:13:34 +0300

    Adding sample of using SPM package

---
+++ YandexLoginSDKSampleSPM/YandexLoginSDKSampleSPM/ErrorViewController.swift
@@ -0,0 +1,20 @@
+import YandexLoginSDK
+
+final class ErrorViewController: BaseViewController {
+    var errorText: String = "" {
+        didSet {
+            reloadSections()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Yandex SDK Sample v. \(YXLSdk.sdkVersion)"
+        reloadSections()
+    }
+
+    private func reloadSections() {
+        let item = ActionCellItem(text: "Activation error", type: .text)
+        sections = [ SectionItem(title: nil, footer: errorText, items: [item]) ]
+    }
+}
